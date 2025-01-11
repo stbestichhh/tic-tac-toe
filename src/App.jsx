f598353c19a2e40c3c7fe601fadf730d38a90819
Kiril Yakymchuk <stbestich@gmail.com>
2025-01-11 13:14:22 +0200

    chore: separated board to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
+export const Square = () => {
+    return <button className="square">X</button>;
+}
+
+export const Row = () => {
+    return (
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+    )
+}
+
 export default function Board() {
-  return (
-      <>
-          <div className='board-row'>
-              <button className="square">X</button>
-              <button className="square">X</button>
-              <button className="square">X</button>
-          </div>
-          <div className='board-row'>
-              <button className="square">X</button>
-              <button className="square">X</button>
-              <button className="square">X</button>
-          </div>
-          <div className='board-row'>
-              <button className="square">X</button>
-              <button className="square">X</button>
-              <button className="square">X</button>
-          </div>
-      </>
+    return (
+        <>
+            <Row />
+            <Row />
+            <Row />
+        </>
   );
 }
